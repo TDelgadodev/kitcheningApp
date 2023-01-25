64e16b3abc737a50e401fc300c5b5e1e7e6bc65a
Thiago <thiagodelgado.dev@gmail.com>
2023-01-25 19:57:12 -0300

    add functionality to show course details dynamically
    add funcionality to show course details dynamically#

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,11 +1,17 @@
 module.exports = {
-    register : (req,res) => {
-        return res.render('users/register')
-    },
-    login : (req,res) => {
-        return res.render('users/login')
-    },
-    profile : (req,res) => {
-        return res.render('users/profile')
-    }
-}
+  register: (req, res) => {
+    return res.render("users/register", {
+      title: "Registro de usuario",
+    });
+  },
+  login: (req, res) => {
+    return res.render("users/login", {
+      title: "Iniciar sesion",
+    });
+  },
+  profile: (req, res) => {
+    return res.render("users/profile", {
+      title: "Perfil de usuario",
+    });
+  },
+};
