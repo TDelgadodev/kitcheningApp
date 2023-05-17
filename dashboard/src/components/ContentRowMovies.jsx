b322c0c932c977853474e9f421d59b7956004e4f
Thiago <thiagodelgado.dev@gmail.com>
2023-05-16 21:54:23 -0300

    Add dashboard using react + vite, added components

---
+++ dashboard/src/components/ContentRowMovies.jsx
@@ -0,0 +1,43 @@
+// eslint-disable-next-line no-unused-vars
+import React, { useEffect, useState } from "react";
+import { Metric } from "./Metric";
+
+export const ContentRowMovies = () => {
+
+  // eslint-disable-next-line no-unused-vars
+  const [state, setState] = useState({
+    courses : {
+      title: "Cursos",
+      color: "primary",
+      value: 0,
+      icon: "fa-film",
+    },
+    chefs : {
+      title: "Chefs",
+      color: "success",
+      value: 0,
+      icon: "fa-award",
+    },
+    users : {
+      title: "Usuarios",
+      color: "warning",
+      value: 0,
+      icon: "fa-user",
+    }
+  });
+
+  useEffect(() => {
+
+  }, []);
+
+  return (
+  <div className="row">
+
+    <Metric {...state.courses}/>
+    <Metric {...state.chefs}/>
+    <Metric {...state.users}/>
+
+
+    </div>
+  );
+};
